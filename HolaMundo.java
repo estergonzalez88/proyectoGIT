13616c3644a67da8f06fafcb44231e97d63df8f0
estergonzalez88 <estergonzalez.alu@iespacomolla.es>
2020-09-27 10:43:49 +0200

    Hola.txt

---
+++ HolaMundo.java
@@ -0,0 +1,5 @@
+public class HolaMundo{
+    public static void main(String[] args){
+        System.out.printtln("Hola 1DAW");
+    }
+}
---
+++ NombreAPP.java
@@ -0,0 +1,11 @@
+/**
+ * Autor: Aquí debe ir tu nombre completo.
+ * Descripción: Aquí debes indicar si el programa compila o no, si funciona como debería (o que partes funcionan y cuales no),
+ * problemas que hayas tenido (si los has conseguido solucionar o no) y otros comentarios que consideres oportunos.
+ * 
+ */
+public class NombreAPP {
+    public static void main(String[] args) {
+        System.out.println("Aquí el programa");
+    } 
+}

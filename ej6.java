dfb984ae01040f0828fd00f7357148935b5ba2c4
estergonzalez88 <estergonzalez.alu@iespacomolla.es>
2020-10-19 18:37:01 +0200

    Modificado ej6

---
+++ ej6.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+public class ej6 {
+    public static void main(String[] args) {
+
+ Scanner teclado = new  Scanner(System.in);
+
+int n1, n2;
+int  opcion = 0;
+
+ System.out.printl("Dame el primer numero entero: ");
+        n1 = input.nextInt();
+
+
+ System.out.printl("Dame el segundo numero entero: ");
+        n2 = input.nextInt();
+
+
+switch int; {
+
+case 1;
+ Si es 1, los números se suman;
+System.out.printl("La suma de: " n1, "+" ,n2, "es", n1 + n2);
+
+case 2;
+Si es 2, se restan;
+System.out.printl("La resta de: " n1, "-" ,n2, "es", n1 - n2);
+
+case 3;
+Si es 3, se multiplican;
+System.out.printl("El producto de: " n1, "*" ,n2, "es", n1 * n2);
+
+case 4;
+Si es 4, se dividen;
+System.out.printl("La division de: " n1, "/" ,n2, "es", n1 / n2);
+
+
+
+default 
+Cualquier otra opción es incorrecta;
+
+
+}
+
+
+
+
+}
+
+
+}
+
+
+
+        
